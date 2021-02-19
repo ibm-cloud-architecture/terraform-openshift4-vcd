bf111d9fc30ee4628bb6897f3159e96d1212afff
Stu Lipshires <stu_lipshires@us.ibm.com>
2021-02-19 20:38:30 +0000

    updated terraform.tvars.example file

--- output.tf
+++ output.tf
@@ -2,10 +2,6 @@ output "kubeadmin_user_info" {
   value = flatten (["kubeadmin", data.local_file.kubeadmin_password.content])
 }  
 
-output "kubeadmin_password" {
-  value = data.local_file.kubeadmin_password.content
-}
-
 output "public_ip" {
   value = var.vcd_edge_gateway["cluster_public_ip"]
 }
