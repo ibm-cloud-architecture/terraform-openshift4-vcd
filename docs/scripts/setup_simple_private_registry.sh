c9d563c4d3696883f0a332a65cbd2301693f57cb
Ashish Deshpande <53223993+aadeshpa@users.noreply.github.com>
2021-06-21 17:01:18 -0400

    Update setup_simple_private_registry.sh

--- docs/scripts/setup_simple_private_registry.sh
+++ docs/scripts/setup_simple_private_registry.sh
@@ -1,28 +1,68 @@
 #Followed this document to script this automated script for setting up simple private registry
 #https://www.redhat.com/sysadmin/simple-container-registry
 
-
-# This parameter is to be set in case of the registry to be setup with existing TLS CERT file like `domain.crt`
-export REGISTRY_SETUP_WITH_EXISTING_TLS_CERTIFICATE="true"
-
-#These parameters needs to be set for setting up the registry
-export registry_dir="/opt/test2_registry"
-export AUTH_DIR="$registry_dir/auth"
-export CERTS_DIR="$registry_dir/certs"
-export DATA_DIR="$registry_dir/data"
-
-# Cert filenames, in case if you dont have one use the name '$HOSTNAME-$REGISTRY_PORT_NUMBER.crt' and '$HOSTNAME-$REGISTRY_PORT_NUMBER.key'
+#############################################################################################
+                 #Parameters that needs to be set before running the script#
+#############################################################################################
+#Parameters for TLS Certificate usage.
+ #This parameter is to be set in case of the registry to be setup with existing TLS CERT file.
+export REGISTRY_SETUP_WITH_EXISTING_TLS_CERTIFICATE="false"
+ #Cert filenames, in case if you dont have one use the name ex: '$HOSTNAME-$REGISTRY_PORT_NUMBER.crt'
 export REGISTRY_HTTP_TLS_KEY_FILENAME="bastion-cp4waiops-registry-cp4waiops-shared-registry-cluster-5004.key"
+ #ex : '$HOSTNAME-$REGISTRY_PORT_NUMBER.key'
 export REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME="bastion-cp4waiops-registry-cp4waiops-shared-registry-cluster-5004.crt"
 
-# Registry accessing credentials
-export registry_username_to_be_created="test_user"
-export registry_password_to_be_set="simplepassword"
-export REGISTRY_PORT_NUMBER="5004"
+#Parameters for registry directory paths
+ #Provide the registry directory ex : '/opt/test2_registry'.
+export registry_dir="/opt/test2_registry"
+ #Provide the Auth directory where registry access crendentials file will be created by the script. ex : '$registry_dir/auth'
+export AUTH_DIR="$registry_dir/auth"
+ #Provide the Certs directory which will be used to either generate self signed certificate, or where you have existing TLS certificate file. ex : '$registry_dir/certs'
+export CERTS_DIR="$registry_dir/certs"
+ #Provide the Data directory which will be used by the registry to store the data. ex: '$registry_dir/data'
+export DATA_DIR="$registry_dir/data"
 
-#This is the auth file tht gets created for your registry credentials
+
+#Parameters for registry credentials to access it.
+ #Username for your registry to access it once it is created.
+export registry_username_to_be_created="test_user"
+ #Password for your registry to access it once it is created.
+export registry_password_to_be_set="simplepassword"
+
+#Parameters for registry name and port number
+ #Registry name that you want to setup
+export REGISTRY_NAME="registry123"
+ #Port number for the the registry to be accessed later once it is created.
+export REGISTRY_PORT_NUMBER="5004"
+#############################################################################################
+
+#This is the auth file that gets created for your registry credentials. This is static and no need to change.
 auth_filename="htpasswd_$REGISTRY_PORT_NUMBER"
 
+
+validate_pre_requisite_tools(){
+
+  echo "[INFO] Checking Pre-requisites:"
+  if podman -v > /dev/null ;then
+     echo "[INFO] Pre-requisite 'Podman' exists"
+  else
+     echo "[ERROR] Pre-requisite 'Podman' does not exists, please install and try again"
+     exit 1
+  fi
+
+  echo "[INFO] Installing httpd-tools"
+  yum install -y podman httpd-tools
+
+}
+
+registry_password_setup(){
+
+  echo "[INFO] Generate credentials for accessing the registry"
+  echo "[INFO] Username as provided : $registry_username_to_be_created"
+  echo "[INFO] Password as provided : $registry_password_to_be_set"
+  htpasswd -bBc $AUTH_DIR/$auth_filename $registry_username_to_be_created $registry_password_to_be_set
+
+}
 
 setup_firewall_for_exposed_port(){
 
@@ -32,10 +72,10 @@ setup_firewall_for_exposed_port(){
 
 }
 
-start_the_registry(){
+start_container_registry(){
 
       echo "[INFO] Starting  the registry"
-      podman run --name registry_$REGISTRY_PORT_NUMBER \
+      podman run --name $REGISTRY_NAME \
       -p $REGISTRY_PORT_NUMBER:5000 \
       -v $DATA_DIR:/var/lib/registry:z \
       -v $AUTH_DIR:/auth:z \
@@ -53,20 +93,49 @@ start_the_registry(){
          echo "[ERROR] Some error while running the registry setup command, please check container logs and try again"
          exit 1
       fi
+
 }
 
+restart_container_registry(){
 
-echo "[INFO] Checking Pre-requisites:"
-if podman -v > /dev/null ;then
-   echo "[INFO] Pre-requisite 'Podman' exists"
-else
-   echo "[ERROR] Pre-requisite 'Podman' does not exists, please install and try again"
-   exit 1
-fi
+     echo "[INFO] Registry container started :"
+     echo "[INFO] ================================================================================================================"
+     podman ps
+     echo "[INFO] ================================================================================================================"
 
-#installing httpd-tools
-echo "[INFO] Installing httpd-tools"
-yum install -y podman httpd-tools
+     registry_container_id=$(podman ps -aqf "name=$REGISTRY_NAME")
+     echo "registry_container_id=$registry_container_id"
+
+     echo "[INFO] Restarting the registry container"
+     podman restart $registry_container_id
+
+     if [ $? -gt 0 ]; then
+       echo "[ERROR] Some issue in restarting your registry, please check container logs and try again"
+       exit 1
+     fi
+}
+
+display_registry_access_details(){
+
+
+     echo "[INFO] Your registry was setup successfully"
+     echo "[INFO] ================================================================================================================"
+     echo "[INFO] Registry Information to access it from client machine"
+     echo "[INFO] ================================================================================================================"
+     echo "[INFO] Registry crt file is present in directory path :  '$CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME' "
+     echo "[INFO] This crt file can be copied to the client machine at path '$CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME' from where you want to login to this registry"
+     echo "[INFO] You need to execute these commands on client where you want to login to this registry"
+     echo "[INFO] 'cp $CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME /etc/pki/ca-trust/source/anchors/'"
+     echo "[INFO] 'update-ca-trust'"
+     echo "[INFO] Verify the ca trust list is updated successfully by command :"
+     echo "[INFO] trust list | grep -i \"$HOSTNAME\""
+     echo "[INFO] ================================================================================================================"
+     echo "[INFO] You can login finally to the registry with command 'GODEBUG=x509ignoreCN=0 podman login $HOSTNAME:$REGISTRY_PORT_NUMBER -u $registry_username_to_be_created -p $registry_password_to_be_set --cert-dir \"/etc/pki/ca-trust/source/anchors/\"' "
+     echo "[INFO] ================================================================================================================"
+
+}
+
+validate_pre_requisite_tools
 
 #Checking if existing TLS cert file 
 if [[ "$REGISTRY_SETUP_WITH_EXISTING_TLS_CERTIFICATE" == "true" ]]; then
@@ -75,15 +144,11 @@ if [[ "$REGISTRY_SETUP_WITH_EXISTING_TLS_CERTIFICATE" == "true" ]]; then
    if [[ ( -f "$CERTS_DIR/$REGISTRY_HTTP_TLS_KEY_FILENAME" ) && ( -f "$CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME" ) ]]; then
       echo "[INFO] The REGISTRY_HTTP_TLS_KEY_FILEPATH=$CERTS_DIR/$REGISTRY_HTTP_TLS_KEY_FILENAME and REGISTRY_HTTP_TLS_CERTIFICATE_FILEPATH=$CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME exists for registry setup."
       
-      echo "[INFO] Generate credentials for accessing the registry once it is setup"
-      echo "[INFO] Username as provided : $registry_username_to_be_created"
-      echo "[INFO] Password as provided : $registry_password_to_be_set"
-
       if [ ! -d "$AUTH_DIR" ]; then
          mkdir -p $AUTH_DIR
       fi
-      htpasswd -bBc $AUTH_DIR/$auth_filename $registry_username_to_be_created $registry_password_to_be_set
-      
+      registry_password_setup
+
       echo "[INFO] The certificate will also have to be trusted by your hosts and clients"
       echo "[INFO] Copying '$CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME' to directory '/etc/pki/ca-trust/source/anchors/'"
       cp $CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME /etc/pki/ca-trust/source/anchors/
@@ -93,44 +158,17 @@ if [[ "$REGISTRY_SETUP_WITH_EXISTING_TLS_CERTIFICATE" == "true" ]]; then
       echo "[INFO] Printing the trust list with hostname=$HOSTNAME"
       echo "Trust List : $(trust list | grep -i $HOSTNAME)"
 
-      start_the_registry
+      start_container_registry
 
+      setup_firewall_for_exposed_port
 
-     setup_firewall_for_exposed_port
-
-     echo "[INFO] Registry container started :"
-     echo "[INFO] ================================================================================================================"
-     podman ps
-     echo "[INFO] ================================================================================================================"
-  
-
-     registry_container_id=$(podman ps --format "{{.ID}}")
-     echo "registry_container_id=$registry_container_id"
-
-     #echo "[INFO] Restarting the registry container"
-     #podman restart $registry_container_id
-
-     if [ $? -gt 0 ]; then
-        echo "[ERROR] Some issue in setting up your registry"
-        exit 1
-     else
-        echo "[INFO] Your registry was setup successfully"
-        echo "[INFO] ================================================================================================================"
-        echo "[INFO] Registry Information to access it from client machine"
-        echo "[INFO] ================================================================================================================"
-        echo "[INFO] Registry crt file is present in directory path :  '$CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME' "
-        echo "[INFO] This crt file can be copied to the client machine at path '$CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME' from where you want to login to this registry"
-        echo "[INFO] You need to execute these commands on client where you want to login to this registry"
-        echo "[INFO] 'cp $CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME /etc/pki/ca-trust/source/anchors/'"
-        echo "[INFO] 'update-ca-trust'"
-        echo "[INFO] Verify the ca trust list is updated successfully by command :"
-        echo "[INFO] trust list | grep -i \"$HOSTNAME\""
-        echo "[INFO] ================================================================================================================"
-        echo "[INFO] You can login finally to the registry with command 'GODEBUG=x509ignoreCN=0 podman login $HOSTNAME:$REGISTRY_PORT_NUMBER -u $registry_username_to_be_created -p $registry_password_to_be_set --cert-dir \"/etc/pki/ca-trust/source/anchors/\"' "
-        echo "[INFO] ================================================================================================================"
-     fi
-
-
+      restart_container_registry
+      if [ $? -gt 0 ]; then
+         echo "[ERROR] Some issue in setting up your registry"
+         exit 1
+      else
+         display_registry_access_details
+      fi
 
    else
       echo "[ERROR] The REGISTRY_HTTP_TLS_KEY_FILEPATH=$REGISTRY_HTTP_TLS_KEY_FILEPATH or REGISTRY_HTTP_TLS_CERTIFICATE_FILEPATH=$REGISTRY_HTTP_TLS_CERTIFICATE_FILEPATH does not exists, please verify if the file paths for these files are correct and if the files are present, and then try again. "
@@ -147,10 +185,7 @@ else
  
   mkdir -p $registry_dir/{auth,certs,data}
   
-  echo "[INFO] Generate credentials for accessing the registry"
-  echo "[INFO] Username as provided : $registry_username_to_be_created"
-  echo "[INFO] Password as provided : $registry_password_to_be_set"
-  htpasswd -bBc $AUTH_DIR/$auth_filename $registry_username_to_be_created $registry_password_to_be_set   
+  registry_password_setup
 
   if [[ ( -f "$CERTS_DIR/$REGISTRY_HTTP_TLS_KEY_FILENAME" ) && ( -f "$CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME" ) ]]; then
      echo "[WARNING] The certificate files $CERTS_DIR/$REGISTRY_HTTP_TLS_KEY_FILENAME and $CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME already exists"
@@ -179,39 +214,16 @@ else
   echo "[INFO] Printing the trust list with hostname=$HOSTNAME"
   echo "Trust List : $(trust list | grep -i $HOSTNAME)"
   
-  start_the_registry
+  start_container_registry
 
   setup_firewall_for_exposed_port
 
-  echo "[INFO] Registry container started :"
-  echo "[INFO] ================================================================================================================"
-  podman ps
-  echo "[INFO] ================================================================================================================"
-
-
-  registry_container_id=$(podman ps --format "{{.ID}}")
-  echo "registry_container_id=$registry_container_id"
-
-  #echo "[INFO] Restarting the registry container"
-  # podman restart $registry_container_id
-
+  restart_container_registry  
   if [ $? -gt 0 ]; then
      echo "[ERROR] Some issue in setting up your registry"
      exit 1
   else
-     echo "[INFO] Your registry was setup successfully"
-     echo "[INFO] ================================================================================================================"
-     echo "[INFO] Registry Information to access it from client machine"
-     echo "[INFO] ================================================================================================================"
-     echo "[INFO] Registry crt file is present in directory path :  '$CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME' "
-     echo "[INFO] This crt file can be copied to the client machine at path '$CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME' from where you want to login to this registry"
-     echo "[INFO] You need to execute these commands on client where you want to login to this registry"
-     echo "[INFO] 'cp $CERTS_DIR/$REGISTRY_HTTP_TLS_CERTIFICATE_FILENAME /etc/pki/ca-trust/source/anchors/'"
-     echo "[INFO] 'update-ca-trust'"
-     echo "[INFO] Verify the ca trust list is updated successfully by command :"
-     echo "[INFO] trust list | grep -i \"$HOSTNAME\""
-     echo "[INFO] ================================================================================================================"
-     echo "[INFO] You can login finally to the registry with command 'GODEBUG=x509ignoreCN=0 podman login $HOSTNAME:$REGISTRY_PORT_NUMBER -u $registry_username_to_be_created -p $registry_password_to_be_set --cert-dir \"/etc/pki/ca-trust/source/anchors/\"' "
-     echo "[INFO] ================================================================================================================"
+     display_registry_access_details
   fi
+
 fi
