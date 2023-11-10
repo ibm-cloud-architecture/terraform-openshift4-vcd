65ff48d9327e4e374949bd26a16f9615037758b8
Stu Lipshires <17011491+slipsibm@users.noreply.github.com>
2023-11-10 17:34:22 -0500

    Update versions.tf to keep ignition provider stable
    change ignition provider to make sure ignition version stay < 3.4.0

--- versions.tf
+++ versions.tf
@@ -13,6 +13,10 @@ terraform {
       source = "vmware/vcd"
       version = "3.7.0"
   }
+    ignition = {
+       source = "community-terraform-providers/ignition"
+       version = "2.1.6"
+    }
   }
   required_version = ">= 0.13"
 }
